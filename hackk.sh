aac2d713bdcb86694c5cfc57dbd018a5f5efadd7
Rohan Handore <Rohanprakash.handore.student.griffith.ie>
2024-05-01 14:00:00 +0100

    updates for 2024-05-01

--- hackk.sh
+++ hackk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Start date
-start_date="2017-01-01"
+start_date="2024-05-01"
 current_date=$start_date
 last_date=$start_date
 
 # Loop for 365 days
-for (( i=1; i<=2920; i++ )); do
+for (( i=1; i<=26; i++ )); do
     # Create a file with the current date as the filename
     mv "${last_date}.txt" "$current_date.txt" || touch "$last_date.txt";
     git add . 
