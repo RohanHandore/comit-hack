115a0eee04dc7db106154908a462018a38239538
Rohan Handore <Rohanprakash.handore.student.griffith.ie>
2024-05-01 14:00:00 +0100

    updates for 2024-05-01

--- hackk.sh
+++ hackk.sh
@@ -3,20 +3,21 @@
 # Start date
 start_date="2024-05-01"
 current_date=$start_date
-last_date=$start_date
 
-# Loop for 365 days
+# Loop for 26 days
 for (( i=1; i<=26; i++ )); do
-    # Create a file with the current date as the filename
-    mv "${last_date}.txt" "$current_date.txt" || touch "$last_date.txt";
-    git add . 
+    # Create a file with the current date as the filename if it doesn't exist
+    if [[ ! -f "$current_date.txt" ]]; then
+        touch "$current_date.txt"
+    fi
+    
+    # Add and commit changes
+    git add .
     git commit -m "updates for $current_date"
     
-    # Set the Git committer date and make a commit
-    #GIT_COMMITTER_DATE="$current_date 14:00 2011 +0100" 
-    git commit --amend --no-edit --date="$current_date 14:00:00"
-
+    # Set the Git committer date and amend the commit
+    GIT_COMMITTER_DATE="$current_date 14:00:00" git commit --amend --no-edit --date="$current_date 14:00:00"
+    
     # Move to the next day
-    last_date=$current_date
-    current_date=$(date -d -v +1d -f "%Y-%m-%d" "$current_date" "+%Y-%m-%d")
+    current_date=$(date -d "$current_date +1 day" "+%Y-%m-%d")
 done
